c01d7ab1ed04e32ce3660b3b351c058e834537f6
Caidy <chen@caidy.cc>
2017-05-09 22:30:04 +0800

    增加：HMI串口屏应用层（hmiHandle.c）

---
+++ source/hmiHandle.c
@@ -0,0 +1,221 @@
+/**
+  ******************************************************************************
+  * File Name          : hmiHandle.c
+  * Description        : HMI串口屏消息处理文件
+  ******************************************************************************
+  */
+  
+/* Includes ------------------------------------------------------------------*/
+#include "hmiHandle.h"
+#include "fsl_common.h"
+
+#include "camera.h"
+#include "blackbox.h"
+#include "hmi.h"
+
+static pageID_e thisPage;
+
+camConf_t camera;
+
+/**
+ * @brief  按钮处理事件
+ * @retval -1,失败 0,成功
+ */
+static int8_t buttonHandle(uint8_t *buf)
+{
+    hmiButton_t *btn = NULL;
+    
+    if (NULL == buf)
+        return -1;
+    btn = (hmiButton_t *) buf;
+    
+    switch (thisPage)
+    {
+        case CAMERA:
+        {
+            if (18 == btn->buttonID)    //保存按钮
+            {
+                Blackbox_WriteConf("cam.cf", &camera, sizeof(camera));
+                CAM_UpdateProfile(&camera);
+            }
+            if (7 == btn->buttonID)     //50hz按钮
+                camera.FPS = 50;
+            if (8 == btn->buttonID)     //75hz按钮
+                camera.FPS = 75;
+            if (9 == btn->buttonID)     //112hz按钮
+                camera.FPS = 112;
+            if (10 == btn->buttonID)    //150hz按钮
+                camera.FPS = 150;
+            break;
+        }
+
+        default:
+        {
+            break;
+        }
+    }
+    
+    return 0;
+}
+
+/**
+ * @brief  滑块处理事件
+ * @retval -1,失败 0,成功
+ */
+static int8_t silderHandle(uint8_t *buf)
+{
+    hmiUser_t *silder = NULL;
+
+    if (NULL == buf)
+        return -1;
+    silder = (hmiUser_t *) buf;
+
+    switch (thisPage)
+    {
+        case CAMERA:
+        {
+            if (13 == silder->widgetID)     //对比度滑块
+                camera.CNST = silder->val;
+            if (17 == silder->widgetID)     //曝光度滑块
+                camera.AEC = silder->val;
+            break;
+        }
+
+        default:
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
+
+/**
+ * @brief  双态按钮处理事件
+ * @retval -1,失败 0,成功
+ */
+static int8_t doubleHandle(uint8_t *buf)
+{
+    hmiUser_t *silder = NULL;
+
+    if (NULL == buf)
+        return -1;
+    silder = (hmiUser_t *) buf;
+
+    switch (thisPage)
+    {
+        case CAMERA:
+        {
+            if (19 == silder->widgetID)     //自动曝光按钮
+                camera.AutoAEC = silder->val;
+            if (20 == silder->widgetID)     //自动白平衡按钮
+                camera.AutoAWB = silder->val;
+            if (21 == silder->widgetID)     //自动增益按钮
+                camera.AutoAGC = silder->val;
+            break;
+        }
+
+        default:
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
+
+/**
+ * @brief  页面处理事件
+ * @retval -1,失败 0,成功
+ */
+static int8_t pageHandle(uint8_t *buf)
+{
+    hmiPage_t *page = NULL;
+
+    if (NULL == buf)
+        return -1;
+    page = (hmiPage_t *) buf;
+
+    thisPage = page->pageID;
+    HMI_TxMsgHandle();
+
+    return 0;
+}
+
+/**
+ * @brief  接收消息处理总事件
+ * @retval -1,失败 0,成功
+ */
+int8_t HMI_RxMsgHandle(uint8_t *buf)
+{
+    hmiPack_t *packge = NULL;
+    
+    if (NULL == buf)
+        return -1;
+    packge = (hmiPack_t *) buf;
+    
+    switch (packge->cmd)
+    {
+        case BUTTON_RTN:
+        {
+            buttonHandle(packge->data);
+            break;
+        }
+
+        case PAGE_RTN:
+        {
+            pageHandle(packge->data);
+            break;
+        }
+
+        case SILDER_RTN:
+        {
+            silderHandle(packge->data);
+            break;
+        }
+
+        case DOUBLE_RTN:
+        {
+            doubleHandle(packge->data);
+            break;
+        }
+
+        default:
+        {
+            break;
+        }
+    }
+    
+    return 0;
+}
+
+/**
+ * @brief  发送消息处理总事件
+ * @retval None
+ */
+void HMI_TxMsgHandle(void)
+{
+    switch(thisPage)
+    {
+        case CAMERA:
+        {
+            Blackbox_ReadConf("cam.cf", &camera, sizeof(camera));
+            HMI_InsertData("fps.val=%d", camera.FPS);
+            HMI_InsertData("cnst.val=%d", camera.CNST);
+            HMI_InsertData("h_cnst.val=%d", camera.CNST);
+            HMI_InsertData("ace.val=%d", camera.AEC);
+            HMI_InsertData("h_ace.val=%d", camera.AEC);
+            HMI_InsertData("bt_ace.val=%d", camera.AutoAEC);
+            HMI_InsertData("bt_awb.val=%d", camera.AutoAWB);
+            HMI_InsertData("bt_agc.val=%d", camera.AutoAGC);
+            break;
+        }
+
+        default:
+        {
+            break;
+        }
+    }
+
+    HMI_SendData();
+}
---
+++ source/hmiHandle.h
@@ -0,0 +1,98 @@
+/**
+  ******************************************************************************
+  * File Name          : hmiHandle.h
+  * Description        : HMI串口屏消息处理头文件
+  ******************************************************************************
+  */
+  
+#ifndef __HANDLE_HMI_H
+#define __HANDLE_HMI_H
+
+#include <stdint.h>
+
+/* 命令类型枚举 */
+typedef enum
+{
+/* 串口指令执行成功或失败的通知格式 */
+    CMD_ERR     = 0x00,
+    CMD_PASS    = 0x01,
+    BTN_ERR     = 0x02,
+    PAGE_ERR    = 0x03,
+    IMG_ERR     = 0x04,
+    FONT_ERR    = 0x05,
+    BAUD_ERR    = 0x11,
+    SCOPE_ERR   = 0x12,
+    VALUE_ERR   = 0x1A,
+    MATH_ERR    = 0x1B,
+    ASSIGN_FAIL = 0x1C,
+    EEPROM_FAIL = 0x1D,
+    VNUM_ERR    = 0x1E,
+    IO_FAIL     = 0x1F,
+    CHAR_ERR    = 0x20,
+    LENGTH_ERR  = 0x23,
+    
+/* 其他数据类型返回 */
+    BUTTON_RTN  = 0x65,
+    PAGE_RTN    = 0x66,
+    TOUCH_RTN   = 0x67,
+    SLPTAP_RTN  = 0x68,
+    STRING_RTN  = 0x70,
+    NUM_RTN     = 0x71,
+    SLEEP_RTN   = 0x86,
+    WAKE_RTN    = 0x87,
+    BOOT_RTN    = 0x88,
+    UPDATE_RTN  = 0x89,
+    
+/* 自定义数据类型返回 */
+    SILDER_RTN  = 0xA0,     //滑块事件
+    DOUBLE_RTN  = 0xA1,     //双态按钮事件
+
+} cmdBit_e;
+
+/* 页面ID枚举 */
+typedef enum
+{
+    BOOT        = 0,
+    MAIN        = 1,
+    CONTROL     = 2,
+    SENSOR      = 3,
+    CAMERA      = 4,
+    
+} __attribute__ ((packed)) pageID_e;
+
+/** HMI数据通用结构 */
+typedef struct {
+    cmdBit_e  cmd;           //返回数据第一位-命令位
+    uint8_t   data[32];      //数据位
+    
+} __attribute__ ((packed)) hmiPack_t;
+
+/** HMI触摸热区事件返回 */
+typedef struct {
+    pageID_e  pageID;
+    uint8_t   buttonID;
+    uint8_t   type;
+    uint8_t   end[3];
+    
+} __attribute__ ((packed)) hmiButton_t;
+
+/** HMI当前页面ID号返回 */
+typedef struct {
+    pageID_e  pageID;
+    uint8_t   end[3];
+    
+} __attribute__ ((packed)) hmiPage_t;
+
+/** HMI自定义事件返回格式 */
+typedef struct {
+    uint32_t  widgetID;
+    uint32_t  val;
+    uint8_t   end[3];
+
+} __attribute__ ((packed)) hmiUser_t;
+
+
+int8_t HMI_RxMsgHandle(uint8_t *buf);
+void HMI_TxMsgHandle(void);
+
+#endif /* __HANDLE_HMI_H */
