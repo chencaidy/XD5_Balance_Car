0c5d672e31f976ac5e74a95670c5e4a2164c828e
Cydmw <513240118@qq.com>
2017-06-15 16:05:28 +0800

    增加了圆环处理和小S的处理。

--- source/imgprocess.h
+++ source/imgprocess.h
@@ -12,6 +12,8 @@ int MedianFilter(void *bitmap);
 
 void img_find_middle(void);
 void img_cross_search(void);
+void img_circle_search(void);
+void img_smalls_search(void);
 
 void Algorithm_Bak(void);
 
